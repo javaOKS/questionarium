4b7a5663ee4986717a20ae3ce1a39cf9fbcc3fdb
Pilkin <okspilkin@gmail.com>
2023-03-18 18:01:50 +0200

    added methods save,delete (homework 12)

--- src/main/java/repository/QuestionRepositoryImpl.java
+++ src/main/java/repository/QuestionRepositoryImpl.java
@@ -17,30 +17,32 @@ public class QuestionRepositoryImpl implements QuestionRepository {
     public QuestionRepositoryImpl(Connection connection) {
         this.connection = connection;
     }
+
     private String findById =
             """
-                SELECT * FROM questions WHERE id = ?
-            """;
+                        SELECT * FROM questions WHERE id = ?
+                    """;
     private String findByTopic =
             """
-                SELECT * FROM questions WHERE topic = ?
-            """;
+                        SELECT * FROM questions WHERE topic = ?
+                    """;
     private String saveQuestion =
             """
-                INSERT INTO questions (text,topic)
-                 VALUES (?,?)
-            """;
+                        INSERT INTO questions (text,topic)
+                         VALUES (?,?)
+                    """;
     private String updateQuestion =
             """
-                UPDATE questions
-                SET text=?, topic=?
-                WHERE id = ?
-                 """;
-    private String deleteQuestion =
+                    UPDATE questions
+                    SET text=?, topic=?
+                    WHERE id = ?
+                     """;
+    private String deleteQuestionById =
             """
-                 DELETE FROM questions
-                  WHERE id = ?
-                    """;
+                    DELETE FROM questions
+                     WHERE id = ?
+                       """;
+
     @Override
     public Question get(int id) {
         try {
@@ -54,10 +56,11 @@ public class QuestionRepositoryImpl implements QuestionRepository {
                     .text(resultSet.getString("text"))
                     .topic(resultSet.getString("topic"))
                     .build();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
     @Override
     public List<Question> getByTopic(String topic) {
         try {
@@ -66,7 +69,7 @@ public class QuestionRepositoryImpl implements QuestionRepository {
             statement.execute();
             ResultSet resultSet = statement.getResultSet();
             List<Question> question = new ArrayList<>();
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 question.add(Question.builder()
                         .id(resultSet.getInt("id"))
                         .text(resultSet.getString("text"))
@@ -75,23 +78,45 @@ public class QuestionRepositoryImpl implements QuestionRepository {
                 );
             }
             return question;
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
     @Override
     public void save(Question question) {
-
+        try {
+            PreparedStatement statement = connection.prepareStatement(saveQuestion);
+            statement.setString(1, question.getText());
+            statement.setString(2, question.getTopic());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void update(Question question) {
-
+        try {
+            PreparedStatement statement = connection.prepareStatement(updateQuestion);
+            statement.setString(1, question.getText());
+            statement.setString(2, question.getTopic());
+            statement.setInt(3, question.getId());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(int id) {
-
+        try {
+            PreparedStatement statement = connection.prepareStatement(deleteQuestionById);
+            statement.setInt(1, id);
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
